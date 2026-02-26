c2803b74166518144e3b63a7e2035e817f080d08
shopanigeria <shopanigeria@gmail.com>
2026-02-26 01:39:53 +0100

    me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import { Toaster } from "@/components/ui/sonner";
 import { satoshi } from "@/fonts/fonts";
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
+import { SmoothScroll } from "@/components/smooth-scroll";
 
 export const metadata: Metadata = {
   title: "Shopa",
   description: "Buy, Sell, Connect",
 };
-
-import { SmoothScroll } from "@/components/smooth-scroll";
 
 export default function RootLayout({
   children,
@@ -16,10 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${satoshi.className} antialiased`}>
-        <SmoothScroll>{children}</SmoothScroll>
-        <Toaster richColors position="top-right" duration={2000} />
+        <Providers>
+          <SmoothScroll>{children}</SmoothScroll>
+          <Toaster richColors position="top-right" duration={2000} />
+        </Providers>
       </body>
     </html>
   );
